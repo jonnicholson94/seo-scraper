a4b09c75cc3c37eb2b956c7807982c237e08f4e4
jonnicholson94 <jon.nicholson94@googlemail.com>
2023-08-06 16:52:45 +0100

    Pushing latest changes. Now exporting the file to excel

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 from lxml import etree
 import requests
+import pandas as pd
 
 url = "https://lovespace.co.uk/sitemap.xml"
 response = requests.get(url, timeout=5)
@@ -10,20 +11,30 @@ xml = etree.tostring(root).decode()
 
 url_list = xml.replace("<loc>", "").replace("</loc>", "").replace("</url>", "").replace("</urlset>", "").split("<url>")
 
-results = []
+titles = []
+descriptions = []
 
-for i in range(1, len(url_list)):
-    result = { "url": url, "title": "", "description": "" }
-    page = requests.get(url_list[1])
+url_list.pop(0)
+
+for i in range(0, len(url_list)):
+    page = requests.get(url_list[i])
     soup = BeautifulSoup(page.content, "html.parser")
     if soup.findAll("meta", attrs={"name": "description"}):
-        result["title"] = soup.find("meta", attrs={"name": "description"}).get("content")
+        titles.append(soup.find("meta", attrs={"name": "description"}).get("content"))
     else:       
-        result["title"] = 'No meta description found'
+        titles.append('No meta description found')
     if soup.findAll("meta", property="og:title"):
-        result["description"] = soup.find("meta", property="og:title").get("content")
+        descriptions.append(soup.find("meta", property="og:title").get("content"))
     else:
-        result["description"] = "No meta description found"
+        descriptions.append("No meta description found")
 
-    results.append(result)
 
+data = {
+    'page_url': url_list,
+    'titles': titles,
+    'descriptions': descriptions
+}
+
+df = pd.DataFrame(data)
+
+df.to_excel("~/Downloads/results.xlsx", index=False)
